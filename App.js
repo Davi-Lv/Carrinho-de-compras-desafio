f457c15c8f2db084d9a082d81d1f7d32a32e2ca9
Davi Mordonho <85120918+Davi-Lv@users.noreply.github.com>
2023-12-07 22:29:45 -0300

    Refactor code to import necessary components and add custom styles

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { View, StyleSheet } from 'react-native'; // Importando o componente View e StyleSheet
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ProductsContextProvider } from './src/context/ProductsContext.js';
@@ -7,6 +7,7 @@ import ProductsScreen from './src/screens/ProductsScreen.js';
 import ProductDetails from './src/screens/ProductDetails.js';
 import CartScreen from './src/screens/CartScreen.js';
 import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 
@@ -14,10 +15,12 @@ function CartButton() {
   const navigation = useNavigation();
 
   return (
-    <Button
-      onPress={() => navigation.navigate('Cart')}
-      title="Cart"
+    <Ionicons
+      name="cart"
+      size={24}
       color="#000"
+      style={styles.cartIcon}
+      onPress={() => navigation.navigate('Cart')}
     />
   );
 }
@@ -32,13 +35,42 @@ function App() {
             component={ProductsScreen}
             options={{
               headerRight: () => <CartButton />,
-            }} />
-          <Stack.Screen name="ProductDetails" component={ProductDetails} />
-          <Stack.Screen name="Cart" component={CartScreen} />
+              headerStyle: styles.headerStyle, // Adicionando estilo personalizado para o cabeçalho
+              headerTitleStyle: styles.headerTitleStyle, // Adicionando estilo personalizado para o título do cabeçalho
+            }}
+          />
+          <Stack.Screen
+            name="ProductDetails"
+            component={ProductDetails}
+            options={{
+              headerStyle: styles.headerStyle, // Adicionando estilo personalizado para o cabeçalho
+              headerTitleStyle: styles.headerTitleStyle, // Adicionando estilo personalizado para o título do cabeçalho
+            }}
+          />
+          <Stack.Screen
+            name="Cart"
+            component={CartScreen}
+            options={{
+              headerStyle: styles.headerStyle, // Adicionando estilo personalizado para o cabeçalho
+              headerTitleStyle: styles.headerTitleStyle, // Adicionando estilo personalizado para o título do cabeçalho
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </ProductsContextProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  headerStyle: {
+    backgroundColor: '#f5f5f5', // Cor de fundo do cabeçalho
+  },
+  headerTitleStyle: {
+    color: '#000', // Cor do texto do título do cabeçalho
+  },
+  cartIcon: {
+    marginRight: 10,
+  },
+});
 
 export default App;
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -2,19 +2,19 @@ import React from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 const ProductItem = ({ product, onSelectProduct }) => (
-  <TouchableOpacity onPress={onSelectProduct} style={styles.container} activeOpacity={0.8}>
+  <TouchableOpacity onPress={onSelectProduct} style={styles.container}>
     <View>
       <Image source={{ uri: product.image }} style={styles.image} />
       <Text style={styles.name}>{product.name}</Text>
-      <Text style={styles.price}>{product.price}</Text>
-    </View>
+        <Text style={styles.price}>{product.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</Text>
+      </View>
   </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
   container: {
     marginVertical: 10,
-    paddingHorizontal: 10,
+    padding: 10,
     backgroundColor: '#fff',
     borderRadius: 5,
     elevation: 2,
